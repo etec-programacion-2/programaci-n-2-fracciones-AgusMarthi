06268ae5f566699ae4f041a3dfe15e85c9b78d85
SIRWARRIOR2017 <joakopastorino@gmail.com>
2025-06-24 13:00:56 -0300

    Etapa 2: Implementación de operadores suma y resta de fracciones

---
+++ app/src/main/kotlin/org/example/Fraccion.kt
@@ -0,0 +1,25 @@
+package org.example
+
+class Fraccion(private var _numerador : Int,private var _denominador: Int) {
+
+    var numerador: Int = _numerador
+        get() = field
+        set(value) { _numerador = value }
+
+    var denominador: Int = _denominador
+        get() = field
+        set(value) { _denominador = value }
+
+
+    init { if (denominador == 0){
+        throw IllegalArgumentException("El denominador no puede ser cero")
+    }}
+
+    override fun toString(): String {
+        return ("$_numerador/$_denominador")
+    }
+
+    fun mostrar(){
+        println(this.toString())
+    }
+    }
